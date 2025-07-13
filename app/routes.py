4081224d51ba4c799442610b4a3b15caae042e7a
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-07-14 00:10:22 +0800

    wip(routes): changed nav links to go through routes

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,18 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+@main.route('/plans')
+def plans():
+    return render_template('plans.html')
+
+@main.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@main.route('/custom')
+def custom():
+    return render_template('custom.html')
