c1c8acbd148494f98a7b717b1f30afb53c63c836
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-03-11 20:35:41 +0800

    Reset: Rewriting project using Laravel

--- TODO.md
+++
@@ -1,8 +0,0 @@
-# To Do
-
-- Make Pages: Index, About, Plans, Shop, Custom
-- Make DB schema
-- Integrate Stripe
-
-## Debug
-
--- config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load from .env
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_LOCATION')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
