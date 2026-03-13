e0d678611695b0d91cf76d80e12a644abe70c442
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-03-13 23:54:51 +0800

    wip(ui)(hero): finished hero html

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -2,11 +2,11 @@
     <div class="px-15 flex justify-between items-center h-20 pb-2">
         <a href="/" class="font-semibold text-4xl">Sparrow Studios</a>
         <nav class="flex space-x-6">
-            <a href="/gallery" class="hover:underline">Gallery</a>
-            <a href="/custom">Custom</a>
-            <a href="/store">Store</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+            <a href="/gallery" class="hover:underline transition">Gallery</a>
+            <a href="/custom" class="hover:underline transition">Custom</a>
+            <a href="/store" class="hover:underline transition">Store</a>
+            <a href="/about" class="hover:underline transition">About</a>
+            <a href="/contact" class="hover:underline transition">Contact</a>
             {{--<a href="">Plans</a>--}}
         </nav>
     </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,17 @@
 <x-layouts.app title="Home">
-    <h1>homepage</h1>
-
-    <h1 class="text-4xl font-bold text-blue-600">
-        Hello Tailwind
-    </h1>
+    <div class="flex justify-between bg-[url('/images/hero.jpg')] bg-cover bg-center h-[500px]">
+        <div class="grid place-items-center m-auto">
+            <main class="p-10 bg-white">
+                <h1 class="text-4xl">Artisan reclaimed wood furniture</h1>
+                <p class="font-bold">lorem ipsum</p>
+                <nav class="flex gap-5">
+                    <button>View Gallery</button>
+                    <button>Custom</button>
+                </nav>
+            </main>
+        </div>
+        <div class="grid place-items-center m-auto">
+            <div class=""></div>
+        </div>
+    </div>
 </x-layouts.app>
