7d84463e7a3f1d94c1241562370e0006787a4e15
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2026-03-13 22:53:09 +0800

    feat (ui): navbar done

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -1,12 +1,13 @@
-<header class="bg-red-700 w-full inline-flex justify-center">
-    <div>
+<header class="bg-black text-white">
+    <div class="px-15 flex justify-between items-center h-20 pb-2">
         <a href="/" class="font-semibold text-4xl">Sparrow Studios</a>
+        <nav class="flex space-x-6">
+            <a href="/gallery" class="hover:underline">Gallery</a>
+            <a href="/custom">Custom</a>
+            <a href="/store">Store</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            {{--<a href="">Plans</a>--}}
+        </nav>
     </div>
-    <aside>
-        <a href="">Gallery</a>
-        <a href="">Custom Orders</a>
-        <a href="">Shop</a>
-        <a href="">About</a>
-        <a href="">Plans</a>
-    </aside>
 </header>
